2150d427b028361781e2993aa2abc64acb8c9093
Leonardo Braz <lhleonardo05@gmail.com>
2021-03-07 15:40:22 -0300

    refactor: remove rota de teste

--- api/src/routes/index.ts
+++ api/src/routes/index.ts
@@ -3,10 +3,6 @@ import { patientsRouter } from "./patients.routes";
 
 const routes = Router();
 
-routes.use("/patients", patientsRouter)
-
-routes.get("/", (req, res) => {
-  return res.status(200).send("Hello!");
-})
+routes.use("/patients", patientsRouter);
 
 export { routes }
