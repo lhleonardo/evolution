f0a07f7cf957deb5613bb9e3bc5f4e5013170969
Leonardo Braz <lhleonardo05@gmail.com>
2021-03-07 21:42:01 -0300

    chore: corrige eslint e prettier

--- api/src/controllers/patients/create-patient/create-patient.controller.ts
+++ api/src/controllers/patients/create-patient/create-patient.controller.ts
@@ -1,13 +1,13 @@
-import { HttpController } from "../../../utils/http/http-controller";
-import { ok } from "../../../utils/http/http-helper";
-import { HttpRequest, HttpResponse } from "../../../utils/http/http-protocols";
-import { CreatePatientDTO } from "./create-patient.dto";
+import { HttpController } from '../../../utils/http/http-controller';
+import { ok } from '../../../utils/http/http-helper';
+import { HttpRequest, HttpResponse } from '../../../utils/http/http-protocols';
+import { CreatePatientDTO } from './create-patient.dto';
 
 export class CreatePatientController
-  implements HttpController<CreatePatientDTO, string> {
+implements HttpController<CreatePatientDTO, string> {
   public async handle(
-    request: HttpRequest<CreatePatientDTO>
+    request: HttpRequest<CreatePatientDTO>,
   ): Promise<HttpResponse<string>> {
-    return ok("Hello Patient");
+    return ok('Hello Patient');
   }
 }
